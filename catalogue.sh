ba2eb806b28981b582641b4679d5dc1d98e2365e
Sindh <sindhugutha88@gmail.com>
2023-07-25 16:47:07 -0500

    modified the required files in the project

--- catalogue.sh
+++ catalogue.sh
@@ -4,7 +4,6 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
-<<<<<<<<<<<< Creating the driectory >>>>>>>>>>>>>>>>
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
