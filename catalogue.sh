41b2fd8a7d996811ea541a7732bd6e58c3c54604
Sindh <sindhugutha88@gmail.com>
2023-07-25 17:08:54 -0500

    modified the required files in the project

--- catalogue.sh
+++ catalogue.sh
@@ -1,18 +1,34 @@
+echo ">>>>>>>>>> create catalogue service <<<<<<<<<<<<<<<<<<"
+
 cp catalogue.service /etc/systemd/system/catalogue.service
+
+echo ">>>>>> create mongodb repos <<<<<<<<<<<"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
+echo ">>>>>> installing nodesource <<<<<<<<<<<"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
+echo ">>>>>> useradd roboshop <<<<<<<<<<<"
 useradd roboshop
+echo ">>>>>> creating app directory <<<<<<<<<<<"
 mkdir /app
+echo ">>>>>> downloading roboshop artifacts <<<<<<<<<<<"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
+echo ">>>>>> unzip the files <<<<<<<<<<<"
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
+
+echo ">>>>>> installing dependencies <<<<<<<<<<<"
 npm install
 
+echo ">>>>>> installing mongodb client <<<<<<<<<<<"
 yum install mongodb-org-shell -y
-systemctl daemon-reload
+
+echo ">>>>>> loading catalogue schema <<<<<<<<<<<"
 mongo --host mongodb.sgdevrobo.online </app/schema/catalogue.js
+echo ">>>>>> start catalogue service <<<<<<<<<<<"
+systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
