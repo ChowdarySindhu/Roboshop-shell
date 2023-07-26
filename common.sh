2954c194d72033262275ef2204a912f661a8e068
Sindh <sindhugutha88@gmail.com>
2023-07-26 12:20:26 -0500

    modified the required files in the project

--- common.sh
+++ common.sh
@@ -1,3 +1,21 @@
+func_appreq(){
+  log=/tmp/roboshop.log
+  echo -e "\e[31m useradd roboshop \e[0m"
+  useradd roboshop &>>${log}
+  echo -e "\e[33m removing the content and reinstalling \e[0m"
+  rm -rf /app &>>${log}
+
+  echo -e "\e[31m creating app directory \e[0m"
+  mkdir /app &>>${log}
+  echo -e "\e[31m downloading roboshop artifacts \e[0m"
+  curl -o /tmp ${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+
+  echo -e "\e[34m unziping the files \e[0m"
+  cd /app
+  unzip  /tmp/${component}.zip &>>${log}
+  cd /app
+}
+
 func_nodejs(){
   log=/tmp/roboshop.log
 echo -e "\e[31m create ${component} service \e[0m"
@@ -10,20 +28,8 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
 echo -e "\e[31m installing nodesource \e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
 yum install nodejs -y &>>${log}
-echo -e "\e[31m useradd roboshop \e[0m"
-useradd roboshop &>>${log}
-echo -e "\e[33m removing the content and reinstalling \e[0m"
-rm -rf /app &>>${log}
 
-echo -e "\e[31m creating app directory \e[0m"
-mkdir /app &>>${log}
-echo -e "\e[31m downloading roboshop artifacts \e[0m"
-curl -o /tmp ${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-
-echo -e "\e[34m unziping the files \e[0m"
-cd /app
-unzip  /tmp/${component}.zip &>>${log}
-cd /app
+func_appreq
 
 echo -e "\e[31m installing dependencies \e[0m"
 npm install &>>${log}
@@ -46,16 +52,7 @@ func_java(){
   cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
   echo -e "\e[31m installing maven \e[0m"
   yum install maven -y &>>${log}
-  echo -e "\e[31m adding user \e[0m"
-  useradd roboshop &>>${log}
-  echo -e "\e[31m creating app directory \e[0m"
-  mkdir /app &>>${log}
-  echo -e "\e[31m downloading artifacts \e[0m"
-  curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-  echo -e "\e[34m unziping the files \e[0m"
-  cd /app &>>${log}
-  unzip /tmp/${component}.zip &>>${log}
-  cd /app &>>${log}
+  fun_appreq
   echo -e "\e[34m cleaning maven package \e[0m"
   mvn clean package &>>${log}
   echo -e "\e[34m moving the ${component} files \e[0m"
