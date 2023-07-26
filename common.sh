e7d9920777960784c57f27f47093db1224073477
Sindh <sindhugutha88@gmail.com>
2023-07-26 11:16:07 -0500

    modified the required files in the project

--- common.sh
+++ common.sh
@@ -4,20 +4,20 @@ echo -e "\e[31m create ${component} service \e[0m"
 
 cp ${component}.service /etc/systemd/system ${component}.service &>>${log}
 
-echo ">>>>>> create mongodb repos <<<<<<<<<<<"
+echo -e "\e[31m create mongodb repos \e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
 
-echo ">>>>>> installing nodesource <<<<<<<<<<<"
+echo -e "\e[31m installing nodesource \e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
 yum install nodejs -y &>>${log}
-echo ">>>>>> useradd roboshop <<<<<<<<<<<"
+echo -e "\e[31m useradd roboshop \e[0m"
 useradd roboshop &>>${log}
 echo -e "\e[33m removing the content and reinstalling \e[0m"
 rm -rf /app &>>${log}
 
-echo ">>>>>> creating app directory <<<<<<<<<<<"
+echo -e "\e[31m creating app directory \e[0m"
 mkdir /app &>>${log}
-echo ">>>>>> downloading roboshop artifacts <<<<<<<<<<<"
+echo -e "\e[31m downloading roboshop artifacts \e[0m"
 curl -o /tmp ${component}.zip https://roboshop-artifacts.s3.amazonaws.com ${component}.zip &>>${log}
 
 echo -e "\e[34m unziping the files \e[0m"
@@ -25,15 +25,15 @@ cd /app
 unzip  /tmp ${component}.zip &>>${log}
 cd /app
 
-echo ">>>>>> installing dependencies <<<<<<<<<<<"
+echo -e "\e[31m installing dependencies \e[0m"
 npm install &>>${log}
 
 echo -e "\e[34m installing mongodb \e[0m"
 yum install mongodb-org-shell -y &>>${log}
 
-echo ">>>>>> loading ${component} schema <<<<<<<<<<<"
+echo -e "\e[31m loading ${component} schema \e[0m"
 mongo --host mongodb.sgdevrobo.online </app/schema ${component}.js &>>${log}
-echo ">>>>>> start ${component} service <<<<<<<<<<<"
+echo -e "\e[31m start ${component} service \e[0m"
 systemctl daemon-reload
 systemctl enable ${component}
 systemctl restart ${component}
